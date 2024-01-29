b8275dd6c445f0c6f19d9af82d02fffa1d5c0eaa
FramiOS <115457023+youabledev@users.noreply.github.com>
2024-01-29 16:46:48 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

--- 240129/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.swift
+++ 240129/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.swift
@@ -1,4 +1,5 @@
 import Foundation
 
-let input = Array(readLine()!.components(separatedBy: " ")).map { Int(String($0))! }
+// let input = Array(readLine()!.components(separatedBy: " ")).map { Int(String($0))! }
+let input = readLine()!.split(separator: " ").map { Int($0)! }
 (1...input[0]).forEach { _ in print(String(repeating: "1", count: input[1])) }
