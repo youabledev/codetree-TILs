3d47b77f383caa92b56252e0653cbb272e16c688
FramiOS <115457023+youabledev@users.noreply.github.com>
2024-01-30 15:58:26 +0900

    Title: 모이자, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240130/모이자/gather.swift
+++ 240130/모이자/gather.swift
@@ -1,12 +1,10 @@
-import Foundation
-
-let _ = readLine()
+let n = Int(readLine()!)!
 let arr = readLine()!.split(separator: " ").map { Int($0)! }
 
 var minSum = 100 * 100 * 100
-for i in 0..<arr.count { // i 번째 집으로 모이면
+for i in 0..<n {
     var tmpSum = 0
-    for j in 0..<arr.count {
+    for j in 0..<n {
         let value = abs(j - i) * arr[j]
         tmpSum += value
     }
