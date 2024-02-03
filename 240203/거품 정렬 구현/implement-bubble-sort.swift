3c5bce51873a072d761e41ea72c564de98fb1659
FramiOS <115457023+youabledev@users.noreply.github.com>
2024-02-03 19:24:06 +0900

    Title: 거품 정렬 구현, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/거품 정렬 구현/implement-bubble-sort.swift
+++ 240203/거품 정렬 구현/implement-bubble-sort.swift
@@ -1,7 +1,7 @@
 let N = Int(readLine()!)!
 var arr = readLine()!.split(separator: " ").map { Int($0)! }
 
-for i in 0..<N {
+for i in 0..<N - 1 {
     for j in 0..<N - 1 - i where arr[j] > arr[j + 1] {
         arr.swapAt(j, j+1)
     }
