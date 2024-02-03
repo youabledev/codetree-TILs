4aa6ef4d5f888d2e743c373f58aa05f04352935e
FramiOS <115457023+youabledev@users.noreply.github.com>
2024-02-03 19:45:52 +0900

    Title: 선택 정렬 구현, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240203/선택 정렬 구현/implement-selection-sort.swift
@@ -0,0 +1,10 @@
+let n = Int(readLine()!)!
+var arr = Array(readLine()!.split(separator: " ").map { Int($0)! })
+
+for i in 0..<arr.count - 1 {
+        for j in 0..<arr.count - 1 - i where arr[j] > arr[j + 1] {
+            arr.swapAt(j, j+1)
+        }
+    }
+    
+    print(arr.map{ String($0) }.joined(separator: " "))
